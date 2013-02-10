ec4a4297d1649bdc7a808c83c31e4e33b62ae0dd
Błażej Czapp <blazej@swiftkey.net>
2013-02-10 21:48:05 +0000

    Increased file index limit.

--- src/net/peerindex/challenge/webcrawler/BcWebCrawler.java
+++ src/net/peerindex/challenge/webcrawler/BcWebCrawler.java
@@ -11,7 +11,7 @@ import java.util.Map;
 public class BcWebCrawler implements WebCrawler {
 
     private static final String FILENAME_PREFIX = "resources/part-m-";
-    private static final int MAX_FILENAME_INDEX = 0;
+    private static final int MAX_FILENAME_INDEX = 167;
     private static final int MAX_NUMBER_OF_CONCURRENT_TASKS = 4;
     private KeyValueStore store;
     private boolean finished = false;
